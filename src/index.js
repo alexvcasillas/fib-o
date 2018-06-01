81b3854ff2197f63ceb9e248b2f3ef7f7cc06f4a
Alex Casillas <alexvcasillas@gmail.com>
2018-06-01 09:54:26 +0200

    Improved fibonacci function and made it safe

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
+const isInt = number => Number(number) === number && number % 1 === 0;
+
 const fib = number => {
+  if (typeof number === 'undefined')
+    throw new Error(
+      `Fibonacci function takes one parameter for the calculations.`
+    );
+  if (typeof number !== 'number')
+    throw new Error(
+      `Fibonacci function takes a natural number as the single parameter for the calculations.`
+    );
+  if (number < 0)
+    throw new Error(
+      `The fibonacci calculations require a non-negative (natural) number. >= 0`
+    );
+  if (!isInt(number))
+    throw new Error(`The fibonacci sequence requires natural numbers.`);
   const cache = {};
 
   function f(number) {
--- test/index.test.js
+++ test/index.test.js
@@ -2,6 +2,43 @@ import test from 'ava';
 
 const { fib } = require('../dist/fib-o');
 
+test(`it shouldn't allow to calculate without the required parameter`, t => {
+  const error = t.throws(() => fib(), Error);
+  t.is(
+    error.message,
+    `Fibonacci function takes one parameter for the calculations.`
+  );
+});
+
+test(`it shouldn't allow to calculate without a number as the parameter`, t => {
+  const error = t.throws(() => fib('10'), Error);
+  t.is(
+    error.message,
+    `Fibonacci function takes a natural number as the single parameter for the calculations.`
+  );
+});
+
+test(`it shouldn't allow to calculate with a negative number as the parameter`, t => {
+  const error = t.throws(() => fib(-10), Error);
+  t.is(
+    error.message,
+    `The fibonacci calculations require a non-negative (natural) number. >= 0`
+  );
+});
+
+test(`it shouldn't allow to calculate without a natural number as the parameter`, t => {
+  const error = t.throws(() => fib(10.2), Error);
+  t.is(error.message, `The fibonacci sequence requires natural numbers.`);
+});
+
+test('it should return 0 when the number is 0', t => {
+  t.is(fib(0), 0);
+});
+
+test('it should return 1 when the number is 1', t => {
+  t.is(fib(1), 1);
+});
+
 test('it should calculate the fibonacci of 50', t => {
   t.is(fib(50), 12586269025);
 });
